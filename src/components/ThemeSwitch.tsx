a0cae5f46854b53fa29e22c27761b2e0a6ffdc7b
584975 <584975@stud.hvl.no>
2023-03-27 22:48:17 +0200

    sync

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,32 +1,36 @@
-import React, { FC, useState } from "react"
-import Switch from "react-switch"
+import React, { FC, useState } from "react";
+import Switch from "react-switch";
 import styled from "styled-components";
 import { DarkMode, LightMode } from "../types/Colors";
 
 const SwitchWrapper = styled.div`
-    display: flex;
-    justify-content: end;
-    padding: 2rem 2rem 2rem 0;
+  display: flex;
+  justify-content: end;
+  padding: 2rem 2rem 2rem 0;
 `;
 
 const ThemeSwitch: FC = () => {
-    const [darkMode, setDarkMode] = useState<boolean>(true)
-    
-    return (
-      <SwitchWrapper>
-        <Switch 
-          checked={darkMode}
-          onChange={(darkMode) => setDarkMode(darkMode)}
-          onColor={DarkMode.controls}
-          onHandleColor={LightMode.background}
-          offColor={LightMode.controls}
-          offHandleColor={LightMode.background}
-          checkedIcon={false}
-          uncheckedIcon={false}
-          activeBoxShadow={darkMode ? `0 0 8px 8px ${DarkMode.controls}` : `0 0 8px 8px ${LightMode.controls}`}
-        />
-      </SwitchWrapper>
-    )
-}
+  const [darkMode, setDarkMode] = useState<boolean>(true);
+
+  return (
+    <SwitchWrapper>
+      <Switch
+        checked={darkMode}
+        onChange={(darkMode: boolean) => setDarkMode(darkMode)}
+        onColor={DarkMode.controls}
+        onHandleColor={LightMode.background}
+        offColor={LightMode.controls}
+        offHandleColor={LightMode.background}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        activeBoxShadow={
+          darkMode
+            ? `0 0 8px 8px ${DarkMode.controls}`
+            : `0 0 8px 8px ${LightMode.controls}`
+        }
+      />
+    </SwitchWrapper>
+  );
+};
 
 export default ThemeSwitch;

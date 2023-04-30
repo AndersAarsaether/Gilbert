0a420d20c863443beba19c132a04b978368df971
584975 <584975@stud.hvl.no>
2023-04-30 14:12:50 +0200

    Fixed config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQG3eR4JCGXgxD6bjIJ_YkVa9OSjKUm_0",
-  authDomain: "scores-app-a8c26.firebaseapp.com",
-  projectId: "scores-app-a8c26",
-  storageBucket: "scores-app-a8c26.appspot.com",
-  messagingSenderId: "166006246251",
-  appId: "1:166006246251:web:e4aec33034cd9bb2dc1a2e",
+  apiKey: "AIzaSyD42qiaf93EMezVipnaMkwddG0ZH1aBNaM",
+  authDomain: "gilbert-ea03b.firebaseapp.com",
+  projectId: "gilbert-ea03b",
+  storageBucket: "gilbert-ea03b.appspot.com",
+  messagingSenderId: "616337740228",
+  appId: "1:616337740228:web:ba4367943472d657a69491",
 };
 
 const app = initializeApp(firebaseConfig);
